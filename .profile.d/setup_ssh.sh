0f44d4e82297b147d95161d95208193cc4457ff9
Peter Crossley <peter.crossley@webtrends.com>
2015-10-07 22:02:42 -0700

    adding profile dir

---
+++ .profile.d/setup_ssh.sh
@@ -0,0 +1,9 @@
+# file: .profile.d/ssh-setup.sh
+
+#!/bin/bash
+echo $0: creating public and private key files
+
+# Create the .ssh directory
+mkdir -p ${HOME}/.ssh
+chmod 700 ${HOME}/.ssh
+cp bin/core-os.pem ${HOME}/.ssh/id_rsa
--- bin/fleetctl_wrapper.sh
+++ bin/fleetctl_wrapper.sh
@@ -1,9 +1,3 @@
-#/bin/bash
-eval "$(ssh-agent)"
-echo "$(env)"
-# Create the .ssh directory
-mkdir -p ${HOME}/.ssh
-chmod 700 ${HOME}/.ssh
-cp bin/core-os.pem ${HOME}/.ssh/id_rsa
-$(./bin/fleetctl --tunnel=fleet.servalot.net:443 --strict-host-key-checking=false $@)
+#/bin/sh
+./bin/fleetctl --tunnel=fleet.servalot.net:443 --strict-host-key-checking=false $@
 

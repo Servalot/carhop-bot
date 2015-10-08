590a0633fa179b1738f769483b9fcf8fb29f9454
Peter Crossley <peter.crossley@webtrends.com>
2015-10-08 00:25:49 -0700

    making sh compat

--- bin/fleetctl_wrapper.sh
+++ bin/fleetctl_wrapper.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 FLEETCTL="fleetctl"
 which $FLEETCTL
-if [[ $? -eq 1 ]]; then
+if test $? -eq 1; then
 	FLEETCTL="./bin/$FLEETCTL"
 fi
 $FLEETCTL --tunnel=fleet.servalot.net:443 --strict-host-key-checking=false $@

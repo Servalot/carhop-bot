5d8b13f8acb7b843ff4cc21057a3ad9e3cd0d987
Peter Crossley <peter.crossley@webtrends.com>
2015-10-07 21:49:19 -0700

    env updare

--- bin/fleetctl_wrapper.sh
+++ bin/fleetctl_wrapper.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 eval "$(ssh-agent)"
-echo "$(export)"
+echo "$(env)"
 # Create the .ssh directory
 mkdir -p ${HOME}/.ssh
 chmod 700 ${HOME}/.ssh
 cp bin/core-os.pem ${HOME}/.ssh/id_rsa
-$(./bin/fleetctl --tunnel=fleet.servalot.net:443 --strict-host-key-checking=false $@)"
+$(./bin/fleetctl --tunnel=fleet.servalot.net:443 --strict-host-key-checking=false $@)
 

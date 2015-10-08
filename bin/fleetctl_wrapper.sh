a5011511d9cec8e688c4669c837075492640651b
Peter Crossley <peter.crossley@webtrends.com>
2015-10-07 21:32:42 -0700

    trying this way

--- bin/fleetctl_wrapper.sh
+++ bin/fleetctl_wrapper.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 eval "$(ssh-agent)"
-echo "$(ssh-add bin/core-os.pem)"
-fleetctl --tunnel=fleet.servalot.net:443 --strict-host-key-checking=false $@
+exec ssh-add bin/core-os.pem &
+exec ./bin/fleetctl --tunnel=fleet.servalot.net:443 --strict-host-key-checking=false $@
 

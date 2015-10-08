60f228f4742527126bbe2b8a912968dff2b6ece3
Peter Crossley <peter.crossley@webtrends.com>
2015-10-07 21:42:37 -0700

    ssh home dir

--- bin/fleetctl_wrapper.sh
+++ bin/fleetctl_wrapper.sh
@@ -1,5 +1,10 @@
 #/bin/bash
 eval "$(ssh-agent)"
-exec ssh-add bin/core-os.pem
+
+# Create the .ssh directory
+mkdir -p ${HOME}/.ssh
+chmod 700 ${HOME}/.ssh
+cp bin/core-os.pem ${HOME}/.ssh/id_rsa
+
 exec ./bin/fleetctl --tunnel=fleet.servalot.net:443 --strict-host-key-checking=false $@
 

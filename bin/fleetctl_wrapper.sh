b7b6a764b2902e9bc67297421370993f4653714d
Peter Crossley <peter.crossley@webtrends.com>
2015-10-07 21:47:16 -0700

    again

--- bin/fleetctl_wrapper.sh
+++ bin/fleetctl_wrapper.sh
@@ -1,10 +1,9 @@
 #/bin/bash
 eval "$(ssh-agent)"
-
+echo "$(export)"
 # Create the .ssh directory
 mkdir -p ${HOME}/.ssh
 chmod 700 ${HOME}/.ssh
 cp bin/core-os.pem ${HOME}/.ssh/id_rsa
-export
-./bin/fleetctl --tunnel=fleet.servalot.net:443 --strict-host-key-checking=false $@
+$(./bin/fleetctl --tunnel=fleet.servalot.net:443 --strict-host-key-checking=false $@)"
 
